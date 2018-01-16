db3d2adab61e7d92e1af4ca04b837c0927b108c9
Dr. Stefan Reisner <stefan.reisner@springer.com>
2018-01-17 00:04:12 +0530

    added pos.js

---
+++ skills/pos.js
@@ -0,0 +1,42 @@
+/*
+
+Botkit Studio Skill module to enhance the "pos" script
+
+*/
+
+var request = require('pos');
+
+module.exports = function(controller) {
+
+    /* Validators */
+    // Fire a function whenever a variable is set because of user input
+    // See: https://github.com/howdyai/botkit/blob/master/docs/readme-studio.md#controllerstudiovalidate
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+    // Validate user input: uri
+    controller.studio.validate('pos','pos_input', function(convo, next) {
+
+        var text = convo.extractResponse('pos_input');
+
+        // test or validate value somehow
+        // can call convo.gotoThread() to change direction of conversation
+
+        console.log('VALIDATE: pos VARIABLE: pos_input=${text}`);
+
+	var words = new pos.Lexer().lex(text);
+	var tagger = new pos.Tagger();
+	var taggedWords = tagger.tag(words);
+	for (i in taggedWords) {
+	    var taggedWord = taggedWords[i];
+	    var word = taggedWord[0];
+	    var tag = taggedWord[1];
+	    console.log(word + " /" + tag);
+	}
+	convo.setVar('pos_output', taggedWords);
+
+        // always call next!
+        next();
+
+    });
+
+};
