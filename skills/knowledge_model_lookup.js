77d2bec844f39450b75421c394cc5c37316006f1
Dr. Stefan Reisner <stefan.reisner@springer.com>
2018-01-17 15:34:17 +0530

    updated knowledge_model_lookup.js

--- skills/knowledge_model_lookup.js
+++ skills/knowledge_model_lookup.js
@@ -15,15 +15,15 @@ module.exports = function(controller) {
 
     controller.studio.beforeThread('knowledge_model_lookup', 'results', function(convo, next) {
 
-	  var uri = convo.extractResponse('uri');
+	  var value = convo.extractResponse('uri');
 	  console.log(`uri=${value}`);
 
-          if (uri.startsWith('http:')) { // looks like a URI
-	  	var encodedUri = encodeURI(uri.substring(1, uri.length-1)); // strip the < > and encode
+          if (value.startsWith('http:')) { // looks like a URI
+	  	var encodedUri = encodeURI(value.substring(1, value.length-1)); // strip the < > and encode
 	  	var requestUrl = `http://models-staging.dev.cf.private.springer.com/km/concept?uri=${encodedUri}`;
 	  	convo.setVar('requestUrl', requestUrl);
 	  	kmLookup(requestUrl).then(function(results) {
-	    		convo.setVar('uri', uri);
+	    		convo.setVar('uri', value);
 	    		convo.setVar('prefLabel', results.prefLabel);
 	    		convo.setVar('altLabels', results.altLabels);
 	    		next();
@@ -35,7 +35,7 @@ module.exports = function(controller) {
           } else { // treat it as a label
 		var branchUri = 'http://km.springer.com/nano-terms/e858247acd17d6a34bd62c59c6d527a7';
 		var encodedBranchUri = encodeURI(branchUri);
-	  	var requestUrl = `http://models-staging.dev.cf.private.springer.com/km?branch=${encodedBranchUri}&label=${uri}`;
+	  	var requestUrl = `http://models-staging.dev.cf.private.springer.com/km?branch=${encodedBranchUri}&label=${value}`;
 	  	convo.setVar('requestUrl', requestUrl);
 	  	kmLookup(requestUrl).then(function(results) {
 	    		convo.setVar('uri', results.uri);
